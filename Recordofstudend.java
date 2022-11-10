d3d60200e71547b19797f3d9ba17274fa4143553
VIKRAM R <116066854+VIKRAMvikram776@users.noreply.github.com>
2022-11-10 21:14:48 +0530

    Add files via upload

---
+++ Recordofstudend.java
@@ -0,0 +1,27 @@
+package thursdaylab;
+
+public class Recordofstudend {
+
+	public static void main(String[] args) {
+		//
+		String  names[]= {"saitharun","lokesh","madhu" }; 
+    	char course[]= {'b','c','a'};
+		int rollno[]= {4,8,9};
+	
+			System.out.println("name"+ names[0]);
+			System.out.println("rollno"+ rollno[0]);
+			System.out.println("course"+ course[0]);
+			
+			System.out.println("name"+ names[1]);
+			System.out.println("rollno"+ rollno[1]);
+			System.out.println("course"+ course[1]);
+			
+			System.out.println("name"+ names[2]);
+			System.out.println("rollno"+ rollno[2]);
+			System.out.println("course:  "+ course[2]);
+			
+		
+		}
+	}
+
+		
---
+++ carconstant.java
@@ -0,0 +1,19 @@
+package thursdaylab;
+
+public class carconstant {
+
+	
+		public static final String NAME = "lamborgini";
+		  public static final String MODEL = "aventador"; 
+		 
+		  public static void main(String[] args) {
+		System.out.println("Name: " + NAME); // result constant variable
+		    System.out.println("Model: " + MODEL);
+		  }
+}
+	
+
+
+	
+
+
